7aa78bf057cbbcffa42407beae76a7ecf06bb3fd
Megan Horsfield <meganthadev@gmail.com>
2020-02-18 03:15:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(people) 
+  if line == 0 
+    puts "The line is currently empty."
+  else 
+    puts "The line is currently: #{other_deli} "  
+  end 
+end  
+
+line(another_deli)
+
+def take_a_number(katz_deli, )
