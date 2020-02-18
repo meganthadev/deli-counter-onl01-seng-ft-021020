6b89d563dd8f7e587fd09395749d4fe93d75c9c6
Megan Horsfield <meganthadev@gmail.com>
2020-02-18 03:45:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
   if katz_deli.empty? 
     puts "The line is currently empty."
   else number = 1
+    katz_deli.each do 
     puts "The line is currently: #{other_deli} "  
   end 
 end  
