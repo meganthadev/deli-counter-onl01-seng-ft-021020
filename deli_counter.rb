33bef1f39fc360d0366f863f30668e14453ff817
Megan Horsfield <meganthadev@gmail.com>
2020-02-18 03:35:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
-def line(people) 
+def line(katz_deli) 
   if line == 0 
     puts "The line is currently empty."
   else 
